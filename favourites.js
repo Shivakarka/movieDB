a7d69222e78378ad8d2039a61715af420c86adb7
Shiva Karka <64298475+Shivakarka@users.noreply.github.com>
2022-01-23 20:53:42 +0530

    initial commit

---
+++ favourites.js
@@ -0,0 +1,105 @@
+
+document.querySelector('.clear').addEventListener('click',handlerClick)
+document.querySelector('ul').addEventListener('click',handlerClick2)
+
+
+//Creating an array to get items from localStorage
+let movieList = [];
+for(let i=0;i<=localStorage.length-1;i++){
+  movieList.push(localStorage.key(i));  
+}
+
+
+//function will loop and create all the movie elements of favourites
+let id="";
+for(let i =0;i<movieList.length;i++){
+  id=movieList[i];
+  getFav(id);
+}
+
+//async function to fetch movie details
+async function getFav(id) {
+try{
+  const URL = `http://www.omdbapi.com/?i=${id}&apikey=102bfbfd`;
+  const res = await fetch(`${URL}`);
+  const data = await res.json();
+  console.log(data);
+  if(data.Response == "True")  addMovieDetails(data);
+ 
+} catch (err) {
+  console.log("Something went wrong: "+ err);
+}
+}
+
+let ul = document.querySelector('ul');
+
+//helper function to add movie details on page
+function addMovieDetails(details){
+  
+  //we will be storing the imdb id of all items as localStorage has keys of this format
+  let movieId=`${details.imdbID}`;
+  let li = document.createElement('li');
+ 
+  //This is template of the data which will be added. Remove button will have class of imdb Id so that when clicked , localStorage removes that particular item
+  li.innerHTML = `
+  <li>
+  <div class="favlist">
+  <div class = "movie-fav-poster">
+      <img src = "${(details.Poster != "N/A") ? details.Poster : "image_not_found.png"}" alt = "movie poster">
+  </div>
+  <div class = "movie-info">
+      <h3 class = "movie-title">${details.Title}</h3>
+      <ul class = "movie-misc-info">
+          <li class = "year">Year: ${details.Year}</li>
+          <li class = "rating">IMDB Rating: ${details.imdbRating}</li>
+          <li class = "released">Released: ${details.Released}</li>
+          <li> <button class="${movieId}" id="del" name="remove"}>Remove</button> </li>
+
+      </ul>
+     
+      
+  </div>
+  </div>
+  </li>
+  `;
+
+  //adding the above template to ul list
+  ul.classList.add('fav-item');
+  ul.appendChild(li);
+  
+  
+}
+
+
+//function to remove movie from local storage
+function removeFav(id){
+   console.log(id);
+  localStorage.removeItem(id);
+}
+
+
+
+//functions which clear the whole/individual list when clicked
+function handlerClick(e){
+  if(e.target.name=='delete'){
+    document.querySelector("ul").innerHTML="";    //deletes all the favourites list items
+  }
+}
+
+
+function removelist(e){
+  
+  let delitem = e.target.parentNode;
+  delitem.parentNode.parentNode.parentNode.remove();
+  
+  let temp = e.target.classList.value;
+  
+  localStorage.removeItem(temp);                //removes the key value in localstorage which has class containing the imdbID of the movie clicked
+}
+
+function handlerClick2(e){
+  if(e.target.name=='remove'){
+    removelist(e);
+  }
+}
+
---
+++ script.js
@@ -0,0 +1,109 @@
+// Movie Titles: https://omdbapi.com/?s=thor&page=1&apikey=102bfbfd
+// Movie details: http://www.omdbapi.com/?i=tt3896198&apikey=102bfbfd
+
+const movieSearchBox = document.getElementById('movie-search-box');
+const searchList = document.getElementById('search-list');
+const resultGrid = document.getElementById('result-grid');
+
+
+//this function trims any spaces for the search input and calls loadMovies function if input is non zero and toggles classes
+function findMovies(){
+    let searchTerm = (movieSearchBox.value).trim();
+    if(searchTerm.length > 0){
+        searchList.classList.remove('hide-search-list');
+        loadMovies(searchTerm);
+    } else {
+        searchList.classList.add('hide-search-list');
+    }
+}
+
+// function to load the movies from API
+async function loadMovies(searchTerm){
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=102bfbfd`;
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    // console.log(data.Search);
+    if(data.Response == "True") displayMovieList(data.Search);
+}
+
+//this functions generates the movie search list items
+function displayMovieList(movies){
+    searchList.innerHTML = "";
+    for(let index = 0; index < movies.length; index++){
+        let movieListItem = document.createElement('div');
+        movieListItem.dataset.id = movies[index].imdbID; // setting movie id
+        movieListItem.classList.add('search-list-item'); //adds class for css
+        if(movies[index].Poster != "N/A")       
+            moviePoster = movies[index].Poster;
+        else 
+            moviePoster = "image_not_found.png";
+
+        //the below code generates the search list items
+        movieListItem.innerHTML = `             
+        <div class = "search-item-thumbnail">
+            <img src = "${moviePoster}">
+        </div>
+        <div class = "search-item-info">
+            <h3>${movies[index].Title}</h3>
+            <p>${movies[index].Year}</p>
+        </div>
+        `;
+        searchList.appendChild(movieListItem);
+      
+    }
+    loadMovieDetails();
+}
+
+//this function displays the full movie details when search item is clicked
+function loadMovieDetails(){
+    const searchListMovies = searchList.querySelectorAll('.search-list-item');
+    searchListMovies.forEach(movie => {
+        movie.addEventListener('click', async () => {
+            searchList.classList.add('hide-search-list');   //hides the search bar
+            movieSearchBox.value = "";                      //changes the search input to empty again
+            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=102bfbfd`); 
+            const movieDetails = await result.json();
+            displayMovieDetails(movieDetails);
+
+            //the below code is regarding the add to favourites button
+            let favButton = document.querySelector("#favid");
+            favButton.addEventListener('click', () => {
+             localStorage.setItem(`${movieDetails.imdbID}`,JSON.stringify(movieDetails.imdbID));  //stores the movie ids in localStorage
+            favButton.innerHTML="Added to Fav";
+            alert("Movie added to Favourites List");                               
+           })    
+        });
+    });
+}
+
+//this is a helper function for the above which adds the template of the movie details that will be displayed
+function displayMovieDetails(movieDetails){
+    resultGrid.innerHTML = `
+    <div class = "movie-poster">
+        <img src = "${(movieDetails.Poster != "N/A") ? movieDetails.Poster : "image_not_found.png"}" alt = "movie poster">
+    </div>
+    <div class = "movie-info">
+        <h3 class = "movie-title">${movieDetails.Title}</h3>
+        <ul class = "movie-misc-info">
+            <li class = "year">Year: ${movieDetails.Year}</li>
+            <li class = "rating">IMDB Rating: ${movieDetails.imdbRating}</li>
+            <li class = "released">Released: ${movieDetails.Released}</li>
+        </ul>
+        <p class = "genre"><b>Genre:</b> ${movieDetails.Genre}</p>
+        <p class = "writer"><b>Writer:</b> ${movieDetails.Writer}</p>
+        <p class = "actors"><b>Actors: </b>${movieDetails.Actors}</p>
+        <p class = "plot"><b>Plot:</b> ${movieDetails.Plot}</p>
+        <p class = "language"><b>Language:</b> ${movieDetails.Language}</p>
+        <p class = "awards"><b><i class = "fas fa-award"></i></b> ${movieDetails.Awards}</p>
+        <button class="fav-btn" id="favid"> Add to Fav </button>
+    </div>
+    `;    
+}
+
+//this event listener hides the search list when clicked outside the searchbar
+window.addEventListener('click', (event) => {
+    if(event.target.className != "form-control"){
+        searchList.classList.add('hide-search-list');
+    }
+});
+
